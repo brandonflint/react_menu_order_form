6b822d7ec7372c88f7e4a865eec9c2e75e68c920
Brandon Flint <flint622@gmail.com>
2017-03-21 20:30:21 -0400

    Remove from order, menu animations, added prop types.

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,11 @@ class App extends React.Component {
 		super();
 
 		this.addFish = this.addFish.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
 		// getinitialState
 		this.state = {
 			fishes: {},
@@ -37,7 +40,6 @@ class App extends React.Component {
 		}
 	}
 
-
 	componentWillUnmount() {
 		base.removeBinding(this.ref);
 	}
@@ -57,6 +59,18 @@ class App extends React.Component {
 		this.setState({ fishes });
 	}
 
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+	}
+
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null; // because of firebase, normally delete fishes[key] would work.
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -70,14 +84,19 @@ class App extends React.Component {
 		order[key] = order[key] + 1 || 1;
 		// set state
 		this.setState({ order });
+	}
 
+	removeFromOrder(key) {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({ order });
 	}
 
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header tagline="Fresh Seafood Market "/>
+					<Header tagline="Fresh Seafood Market"/>
 					<ul className="list-of-fishes">
 						{
 							Object.keys(this.state.fishes)
@@ -85,11 +104,26 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order} 
+					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
+				/>
+				<Inventory
+					fishes={this.state.fishes}
+					addFish={this.addFish}
+					loadSamples={this.loadSamples}
+					updateFish={this.updateFish}
+					removeFish={this.removeFish}
+				/>
 			</div>
 		)
 	}
+}
+
+App.propTypes = {
+	params: React.PropTypes.object.isRequired
 }
 
 export default App; 
